c90f12ebf9b124a1c4e8f849954fc5b154087a6d
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-10-08 14:24:38 +0200

    simplified serialization API usage

--- contracts/Core/ZBGameModeSerialization.sol
+++ contracts/Core/ZBGameModeSerialization.sol
@@ -1,9 +1,11 @@
 pragma solidity ^0.4.24;
 
 import "./ZB/ZBEnum.sol";
-import "./../3rdParty/Seriality/Seriality.sol";
+import "./../3rdParty/Seriality/BytesToTypes.sol";
+import "./../3rdParty/Seriality/SizeOf.sol";
+import "./../3rdParty/Seriality/TypesToBytes.sol";
 
-contract ZBGameModeSerialization is ZBEnum, Seriality {
+library ZBGameModeSerialization {
     struct PlayerState {
         uint8 defense;
         uint8 goo;
@@ -15,57 +17,61 @@ contract ZBGameModeSerialization is ZBEnum, Seriality {
         PlayerState[] playerStates;
     }
 
-    function deserializeGameState(bytes serializedGameState) internal pure returns (GameState) {
+    struct GameStateSerializedChanges {
+        bytes buffer;
+        uint offset;
+    }
+
+    function initWithSerializedData(GameState memory self, bytes serializedGameState) internal pure {
         uint offset = serializedGameState.length;
 
-        GameState memory gameState;
+        self.id = BytesToTypes.bytesToInt64(offset, serializedGameState);
+        offset -= SizeOf.sizeOfInt(64);
 
-        gameState.id = bytesToInt64(offset, serializedGameState);
-        offset -= sizeOfInt(64);
+        self.currentPlayerIndex = BytesToTypes.bytesToUint8(offset, serializedGameState);
+        offset -= SizeOf.sizeOfInt(8);
 
-        gameState.currentPlayerIndex = bytesToUint8(offset, serializedGameState);
-        offset -= sizeOfInt(8);
+        self.playerStates = new PlayerState[](2);
+        for (uint i = 0; i < self.playerStates.length; i++) {
+            self.playerStates[i].defense = BytesToTypes.bytesToUint8(offset, serializedGameState);
+            offset -= SizeOf.sizeOfInt(8);
 
-        gameState.playerStates = new PlayerState[](2);
-        for (uint i = 0; i < gameState.playerStates.length; i++) {
-            gameState.playerStates[i].defense = bytesToUint8(offset, serializedGameState);
-            offset -= sizeOfInt(8);
-
-            gameState.playerStates[i].goo = bytesToUint8(offset, serializedGameState);
-            offset -= sizeOfInt(8);
+            self.playerStates[i].goo = BytesToTypes.bytesToUint8(offset, serializedGameState);
+            offset -= SizeOf.sizeOfInt(8);
         }
-
-        return gameState;
     }
 
-    function startGameStateChangeAction(bytes buffer, uint offset, GameStateChangeAction action) internal pure returns (uint) {
-        uint newOffset = offset;
-        intToBytes(newOffset, uint32(action), buffer);
-        newOffset -= sizeOfInt(32);
-        return newOffset;
+    function getBytes(GameStateSerializedChanges memory self) internal pure returns (bytes) {
+        return self.buffer;
     }
 
-    function changePlayerDefense(bytes buffer, uint offset, uint8 playerIndex, uint8 defense) internal pure returns (uint) {
-        uint newOffset = startGameStateChangeAction(buffer, offset, GameStateChangeAction.SetPlayerDefense);
-
-        intToBytes(newOffset, playerIndex, buffer);
-        newOffset -= sizeOfInt(8);
-
-        intToBytes(newOffset, defense, buffer);
-        newOffset -= sizeOfInt(8);
-
-        return newOffset;
+    function init(GameStateSerializedChanges memory self, uint bufferSize) internal pure {
+        self.buffer = new bytes(bufferSize);
+        self.offset = bufferSize;
     }
 
-    function changePlayerGoo(bytes buffer, uint offset, uint8 playerIndex, uint8 goo) internal pure returns (uint) {
-        uint newOffset = startGameStateChangeAction(buffer, offset, GameStateChangeAction.SetPlayerGoo);
+    function changePlayerDefense(GameStateSerializedChanges memory self, uint8 playerIndex, uint8 defense) internal pure returns (uint) {
+        serializeStateGameStateChangeAction(self, ZBEnum.GameStateChangeAction.SetPlayerDefense);
 
-        intToBytes(newOffset, playerIndex, buffer);
-        newOffset -= sizeOfInt(8);
+        TypesToBytes.intToBytes(self.offset, playerIndex, self.buffer);
+        self.offset -= SizeOf.sizeOfInt(8);
 
-        intToBytes(newOffset, goo, buffer);
-        newOffset -= sizeOfInt(8);
+        TypesToBytes.intToBytes(self.offset, defense, self.buffer);
+        self.offset -= SizeOf.sizeOfInt(8);
+    }
 
-        return newOffset;
+    function changePlayerGoo(GameStateSerializedChanges memory self, uint8 playerIndex, uint8 goo) internal pure {
+        serializeStateGameStateChangeAction(self, ZBEnum.GameStateChangeAction.SetPlayerGoo);
+
+        TypesToBytes.intToBytes(self.offset, playerIndex, self.buffer);
+        self.offset -= SizeOf.sizeOfInt(8);
+
+        TypesToBytes.intToBytes(self.offset, goo, self.buffer);
+        self.offset -= SizeOf.sizeOfInt(8);
+    }
+
+    function serializeStateGameStateChangeAction(GameStateSerializedChanges memory self, ZBEnum.GameStateChangeAction action) private pure {
+        TypesToBytes.intToBytes(self.offset, uint32(action), self.buffer);
+        self.offset -= SizeOf.sizeOfInt(32);
     }
 }
--- test/TestSerialization.sol
+++ test/TestSerialization.sol
@@ -3,24 +3,32 @@ pragma solidity ^0.4.24;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Core/ZBGameModeSerialization.sol";
+import "../contracts/3rdParty/Seriality/BytesToTypes.sol";
+import "../contracts/3rdParty/Seriality/SizeOf.sol";
+import "../contracts/3rdParty/Seriality/TypesToBytes.sol";
 
-contract TestSerialization is ZBGameModeSerialization {
+contract TestSerialization {
+    using ZBGameModeSerialization for ZBGameModeSerialization.GameStateSerializedChanges;
+    using ZBGameModeSerialization for ZBGameModeSerialization.GameState;
+
     function testDeserializeInts() public {
         bytes memory buffer = hex'00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000400000003000201';
         uint offset = buffer.length;
-        Assert.equal(int(bytesToInt8(offset, buffer)), int(1), "");
-        offset -= sizeOfInt(8);
-        Assert.equal(int(bytesToInt16(offset, buffer)), int(2), "");
-        offset -= sizeOfInt(16);
-        Assert.equal(int(bytesToInt32(offset, buffer)), int(3), "");
-        offset -= sizeOfInt(32);
-        Assert.equal(int(bytesToInt64(offset, buffer)), int(4), "");
-        offset -= sizeOfInt(64);
+        Assert.equal(int(BytesToTypes.bytesToInt8(offset, buffer)), int(1), "");
+        offset -= SizeOf.sizeOfInt(8);
+        Assert.equal(int(BytesToTypes.bytesToInt16(offset, buffer)), int(2), "");
+        offset -= SizeOf.sizeOfInt(16);
+        Assert.equal(int(BytesToTypes.bytesToInt32(offset, buffer)), int(3), "");
+        offset -= SizeOf.sizeOfInt(32);
+        Assert.equal(int(BytesToTypes.bytesToInt64(offset, buffer)), int(4), "");
+        offset -= SizeOf.sizeOfInt(64);
     }
 
     function testDeserializeGameState() public {
         bytes memory buffer = hex'01140114000000000000000005';
-        GameState memory gameState = deserializeGameState(buffer);
+        ZBGameModeSerialization.GameState memory gameState;
+        gameState.initWithSerializedData(buffer);
+
         Assert.equal(gameState.id, int64(5), "");
         Assert.equal(int(gameState.currentPlayerIndex), uint8(0), "");
         Assert.equal(int(gameState.playerStates[0].defense), int(20), "");

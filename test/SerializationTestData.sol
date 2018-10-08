052a36390ddc0208bcc17be1b1d2d5e038540053
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-10-08 14:24:38 +0200

    simplified serialization API usage

--- test/SerializationTestData.sol
+++ test/SerializationTestData.sol
@@ -3,29 +3,35 @@ pragma solidity ^0.4.24;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Core/ZBGameModeSerialization.sol";
+import "../contracts/3rdParty/Seriality/BytesToTypes.sol";
+import "../contracts/3rdParty/Seriality/SizeOf.sol";
+import "../contracts/3rdParty/Seriality/TypesToBytes.sol";
 
-contract SerializationTestData is ZBGameModeSerialization {
-    function serializeInts() public returns (bytes) {
+contract SerializationTestData {
+    using ZBGameModeSerialization for ZBGameModeSerialization.GameStateSerializedChanges;
+    using ZBGameModeSerialization for ZBGameModeSerialization.GameState;
+
+    function serializeInts() public pure returns (bytes) {
         bytes memory buffer = new bytes(64);
         uint offset = 64;
-        intToBytes(offset, int8(1), buffer);
-        offset -= sizeOfInt(8);
-        intToBytes(offset, int16(2), buffer);
-        offset -= sizeOfInt(16);
-        intToBytes(offset, int32(3), buffer);
-        offset -= sizeOfInt(32);
-        intToBytes(offset, int64(4), buffer);
-        offset -= sizeOfInt(64);
+        TypesToBytes.intToBytes(offset, int8(1), buffer);
+        offset -= SizeOf.sizeOfInt(8);
+        TypesToBytes.intToBytes(offset, int16(2), buffer);
+        offset -= SizeOf.sizeOfInt(16);
+        TypesToBytes.intToBytes(offset, int32(3), buffer);
+        offset -= SizeOf.sizeOfInt(32);
+        TypesToBytes.intToBytes(offset, int64(4), buffer);
+        offset -= SizeOf.sizeOfInt(64);
         return buffer;
     }
 
-    function serializeGameStateChangeActions() public returns (bytes) {
-        bytes memory buffer = new bytes(64);
-        uint offset = buffer.length;
-        offset = changePlayerDefense(buffer, offset, 0, 5);
-        offset = changePlayerDefense(buffer, offset, 1, 6);
-        offset = changePlayerGoo(buffer, offset, 0, 7);
-        offset = changePlayerGoo(buffer, offset, 1, 8);
-        return buffer;
+    function serializeGameStateChangeActions() public pure returns (bytes) {
+        ZBGameModeSerialization.GameStateSerializedChanges memory changes;
+        changes.init(64);
+        changes.changePlayerDefense(0, 5);
+        changes.changePlayerDefense(1, 6);
+        changes.changePlayerGoo(0, 7);
+        changes.changePlayerGoo(1, 8);
+        return changes.getBytes();
     }
 }

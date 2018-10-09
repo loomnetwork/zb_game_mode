5236e4b2f141c2cfd1549f7c8d84c2282e27f1a4
Serhii Yolkin <serhii.yolkin@lostpolygon.com>
2018-10-09 04:37:03 +0200

    Initial custom UI serialization

--- test/serialization.test.js
+++ test/serialization.test.js
@@ -23,11 +23,31 @@ contract('SerializationTestData', accounts => {
         assert.deepEqual(slice, [ 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 3, 0, 2, 1 ])
     })
 
+    it('Contract should serialize strings', async () => {
+        serializationTestData.address.should.not.be.null
+
+        const res = await serializationTestData.serializeShortString.call()
+        assert.ok(res.endsWith("436f6f6c20427574746f6e000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b"))
+    })
+
+    it('Contract should serialize long strings', async () => {
+        serializationTestData.address.should.not.be.null
+
+        const res = await serializationTestData.serializeLongString.call()
+        assert.ok(res.endsWith("6f6c20427574746f6e20300000000000000000000000000000000000000000003720436f6f6c20427574746f6e203820436f6f6c20427574746f6e203920436f746f6e203520436f6f6c20427574746f6e203620436f6f6c20427574746f6e2020427574746f6e203320436f6f6c20427574746f6e203420436f6f6c20427574436f6f6c20427574746f6e203120436f6f6c20427574746f6e203220436f6f6c000000000000000000000000000000000000000000000000000000000000008b"))
+    })
+
     it('Contract should serialize state change actions', async () => {
         serializationTestData.address.should.not.be.null
 
         const res = await serializationTestData.serializeGameStateChangeActions.call()
-
         assert.ok(res.endsWith("080100000002070000000002060100000001050000000001"))
+    })
+
+    it('Contract should serialize custom UI', async () => {
+        serializationTestData.address.should.not.be.null
+
+        const res = await serializationTestData.serializeCustomUi.call()
+        assert.ok(res.endsWith("736f6d6546756e6374696f6e0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c436c69636b204d65000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000800000096000000c80000001e0000001900000002536f6d65205665727920436f6f6c207465787421000000000000000000000000000000000000000000000000000000000000000000000000000000000000001400000096000000c8000000e60000001900000001"))
     })
 })
